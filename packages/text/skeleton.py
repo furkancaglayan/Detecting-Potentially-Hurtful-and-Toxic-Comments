d10ab6de0fb3344de5201eb51a006212aca02261
fcaglayan <furkancaglayan15@hotmail.com>
2020-01-02 20:00:37 +0300

    converted utilities to classes.

--- packages/text/skeleton.py
+++ packages/text/skeleton.py
@@ -1,53 +1,61 @@
+import pandas
+
 from packages.text.textutilities import load_data
 import time
 
 
 class Skeleton(object):
-    def __init__(self):
+    def __init__(self, keys, random_state):
         self.df_path = ""
         self.df = None
         self.column_name = ""
-        self.text = None
-        self.methods = []
         self.classifiers = []
         self.progress = ""
         self.data_size = 0
         self.start_time = time.time()
+        self.utilities = None
+        self.keys = keys
+        self.random_state = random_state
 
-    def _create(self, dataframe_path, step, total_steps, related_column='comment_text'):
+    def _create(self, dataframe_path, related_column='comment_text'):
         self.df_path = dataframe_path
-        self.df = load_data(dataframe_path, step, total_steps)
+        self.df = load_data(dataframe_path)
         self.column_name = related_column
         self.data_size = len(self.df)
-        self.text = self.df[related_column]
 
-    def build(self, methods, df_path):
-        self.methods = methods
-        self._create(df_path, 1, len(methods) + 1)
-        for i, method in enumerate(methods):
-            args = {'data': self.text, 'step': i + 2, 'total_steps': len(methods) + 1}
-            self.text = method(**args)
-        self.df[self.column_name] = self.text
+    def build(self, utilities, df_path):
+        self.utilities = utilities
+        self._create(df_path)
+        for i, utility in enumerate(utilities):
+            args = {'df': self.df, 'column': self.column_name}
+            utility.what(i, len(utilities))
+            self.df = utility.apply(**args)
         self.df.dropna(inplace=True, how="any")
         self.data_size = len(self.df)
 
-    def classify(self, classifiers, keys: list):
+    def classify(self, classifiers):
         self.classifiers = classifiers
 
-        X = self.text
+        X = self.df[self.column_name]
         for i, clf in enumerate(self.classifiers):
-            clf.keys = keys
+            clf.keys = self.keys
             print("[Classifier {}/{}   Fitting data over {}...]".format(i + 1, len(self.classifiers), clf.name))
             self.progress += "[Classifier {}/{}   Fitting data over {}...]\n".format(i + 1, len(self.classifiers),
                                                                                      clf.name)
-            for key in keys:
+            for key in self.keys:
                 y = self.df[key]
                 clf.fit(X, y)
                 clf.predict(key, X, self.df[key])
             self.progress += clf.debug()
 
     def save_progress(self, path):
-        self.progress+="\n\n-----------End------------\nTime taken: {} seconds".format(time.time()-self.start_time)
+        self.progress += "\n\n-----------End------------\nTime taken: {} seconds".format(time.time() - self.start_time)
         summary_out = open(path, "w")
         summary_out.write(self.progress)
         summary_out.close()
+
+    def split_by_keys(self, n_category):
+        df_splits = {}
+        for each_key in self.keys:
+            df_splits[each_key] = self.df[self.df[each_key] == 1].sample(n=n_category, random_state=self.random_state)
+        return df_splits
--- packages/text/textutilities.py
+++ packages/text/textutilities.py
@@ -1,121 +1,168 @@
 import random
-
 import pandas as pd
 
 
-def load_data(data: str, step, total_steps):
-    print("[Step {}/{}   Reading File...]".format(step, total_steps))
+class _Utility(object):
+    def __init__(self):
+        pass
+
+    def apply(self, df, column):
+        pass
+
+    def what(self, index, size):
+        pass
+
+
+class Sampler(_Utility):
+    def __init__(self, _min=2, _max=5):
+        self._min = _min
+        self._max = _max
+        super().__init__()
+
+    def print_samples(self, df, column):
+        n = int(random.randrange(self._min, self._max))
+        l = len(df)
+        for i in range(n):
+            random_int = int(random.randrange(0, l - 1))
+            row = df[column].iloc[random_int]
+            if row is None:
+                continue
+            print("Sample {}: \n".format(i + 1) + row)
+        return df
+
+    def apply(self, df, column):
+        self.print_samples(df, column)
+        return df
+
+    def what(self, index, size):
+        print("[Step {}/{}   Sampling data...]".format(index + 1, size))
+
+
+class TextCleaner(_Utility):
+    def __init__(self):
+        super().__init__()
+
+    def apply(self, df, column):
+        return self.clean_text(df, column)
+
+    def clean_text(self, df, column):
+        data = df[column]
+        # remove newline
+        data = data.apply(lambda x: str(x).replace('\n', ' '))
+        # replace i'm with i am
+        data = data.apply(lambda x: str(x).replace('\'m', ' am'))
+        # replace don't and isn't etc
+        data = data.apply(lambda x: str(x).replace('n\'t', ' not'))
+        # handle can't to can not
+        data = data.apply(lambda x: str(x).replace('can\'t', 'can not'))
+        data = data.apply(lambda x: str(x).replace('\'d', ' would'))
+        # remove : from dates and time mostly
+        data = data.apply(lambda x: str(x).replace(':', ' '))
+        # remove hashtag
+        data = data.apply(lambda x: str(x).replace('#', ' '))
+        data = data.apply(lambda x: str(x).replace('_', ' '))
+        data = data.apply(lambda x: str(x).replace('\'s', ''))
+        data = data.apply(lambda x: str(x).replace('-', ' '))
+        data = data.apply(lambda x: str(x).replace('"', ''))
+        data = data.apply(lambda x: str(x).replace('\'', ' '))
+        data = data.apply(lambda x: str(x).replace('|', ' '))
+        data = data.apply(lambda x: str(x).replace('!', ''))
+        data = data.apply(lambda x: str(x).replace('?', ''))
+        data = data.apply(lambda x: str(x).replace('\\', ''))
+        data = data.apply(lambda x: str(x).replace('/', ' '))
+        data = data.apply(lambda x: str(x).replace('[', ''))
+        data = data.apply(lambda x: str(x).replace(']', ''))
+        data = data.apply(lambda x: str(x).replace(')', ''))
+        data = data.apply(lambda x: str(x).replace('(', ''))
+        data = data.apply(lambda x: str(x).replace('{', ''))
+        data = data.apply(lambda x: str(x).replace('}', ''))
+        data = data.apply(lambda x: str(x).replace('.', ''))
+        data = data.apply(lambda x: str(x).replace(',', ''))
+        data = data.apply(lambda x: str(x).replace('•', ''))
+        data = data.apply(lambda x: str(x).replace('   ', ' '))
+        data = data.apply(lambda x: str(x).replace('  ', ' '))
+        data = data.apply(lambda x: str(x).replace('=', ''))
+        data = data.apply(lambda x: str(x).replace('+', ''))
+
+        data = data.apply(lambda x: '' if type(x) is int or x is float else x)
+
+        def remove_if_num(x: str) -> str:
+            arr = x.split(' ')
+            for i in range(len(arr)):
+                if arr[i].isdigit():
+                    x = x.replace(arr[i], '')
+            return x
+
+        data = data.apply(lambda x: remove_if_num(x).strip())
+
+        data = data.str.lower()
+        df[column] = data
+        return df
+
+    @staticmethod
+    def clean_sample(txt="Sample Text"):
+        txt = str(txt).replace('\n', ' ')
+        txt = str(txt).replace('\'m', ' am')
+        txt = txt.replace('n\'t', ' not')
+        txt = txt.replace('can\'t', 'can not')
+        txt = txt.replace('\'d', ' would')
+        txt = txt.replace(':', ' ')
+        txt = txt.replace('#', ' ')
+        txt = txt.replace('_', ' ')
+        txt = txt.replace('\'s', '')
+        txt = txt.replace('-', ' ')
+        txt = txt.replace('"', '')
+        txt = txt.replace('\'', ' ')
+        txt = txt.replace('!', '')
+        txt = txt.replace('?', '')
+        txt = txt.replace('/', ' ')
+        txt = txt.replace('\'', '')
+        txt = txt.replace('[', '')
+        txt = txt.replace(']', '')
+        txt = txt.replace('{', '')
+        txt = txt.replace('}', '')
+        txt = txt.replace('(', '')
+        txt = txt.replace(')', '')
+        txt = txt.replace('.', '')
+        txt = txt.replace(',', '')
+        txt = txt.replace('•', '')
+        txt = txt.replace('   ', ' ')
+        txt = txt.replace('  ', ' ')
+        txt = txt.replace('=', '')
+        txt = txt.replace('+', '')
+        txt = txt.replace('|', ' ')
+
+        return txt.lower().strip()
+
+    def what(self, index, size):
+        print("[Step {}/{}   Cleaning data...]".format(index + 1, size))
+
+
+class Trimmer(_Utility):
+    def __init__(self, threshold=10):
+        self.threshold = threshold
+        super().__init__()
+
+    def trim_corpus(self, df, column):
+        data = df[column]
+        for i, row in enumerate(data):
+            if len(row.split(' ')) <= self.threshold:
+                data.iloc[i] = None
+        return data
+
+    def apply(self, df, column):
+        text = self.trim_corpus(df, column)
+        df[column] = text
+        return df
+
+    def what(self, index, size):
+        print("[Step {}/{}  Trimming data...]".format(index + 1, size))
+
+
+def load_data(data: str):
     try:
         df = pd.read_csv(data)
         return df
     except FileNotFoundError as e:
         print(e.strerror)
         return None
-
-
-def sample(data: pd.DataFrame = None, key_name='comment_text', step=0, total_steps=0, n=5):
-    print("[Step {}/{}   Sampling data...]\n".format(step, total_steps))
-    l = len(data)
-    for i in range(n):
-        random_int = int(random.randrange(0, l - 1))
-        row = data.iloc[random_int]
-        if row is None:
-            continue
-        print("Sample {}: \n".format(i + 1) + row)
-    return data
-
-
-def clean_text(data: pd.DataFrame = None, step=0, total_steps=0):
-    """
-    Takes the related dataframe *column* and removes punctuation from it.
-    :param step:
-    :param total_steps:
-    :param data:
-    :return:
-    """
-    print("[Step {}/{}   Cleaning data...]".format(step, total_steps))
-    # remove newline
-    data = data.apply(lambda x: str(x).replace('\n', ' '))
-    # replace i'm with i am
-    data = data.apply(lambda x: str(x).replace('\'m', ' am'))
-    # replace don't and isn't etc
-    data = data.apply(lambda x: str(x).replace('n\'t', ' not'))
-    # handle can't to can not
-    data = data.apply(lambda x: str(x).replace('can\'t', 'can not'))
-    data = data.apply(lambda x: str(x).replace('\'d', ' would'))
-    # remove : from dates and time mostly
-    data = data.apply(lambda x: str(x).replace(':', ' '))
-    # remove hashtag
-    data = data.apply(lambda x: str(x).replace('#', ' '))
-    data = data.apply(lambda x: str(x).replace('_', ' '))
-    data = data.apply(lambda x: str(x).replace('\'s', ''))
-    data = data.apply(lambda x: str(x).replace('-', ' '))
-    data = data.apply(lambda x: str(x).replace('"', ''))
-    data = data.apply(lambda x: str(x).replace('\'', ' '))
-    data = data.apply(lambda x: str(x).replace('|', ' '))
-    data = data.apply(lambda x: str(x).replace('!', ''))
-    data = data.apply(lambda x: str(x).replace('?', ''))
-    data = data.apply(lambda x: str(x).replace('\\', ''))
-    data = data.apply(lambda x: str(x).replace('/', ' '))
-    data = data.apply(lambda x: str(x).replace('[', ''))
-    data = data.apply(lambda x: str(x).replace(']', ''))
-    data = data.apply(lambda x: str(x).replace(')', ''))
-    data = data.apply(lambda x: str(x).replace('(', ''))
-    data = data.apply(lambda x: str(x).replace('{', ''))
-    data = data.apply(lambda x: str(x).replace('}', ''))
-    data = data.apply(lambda x: str(x).replace('.', ''))
-    data = data.apply(lambda x: str(x).replace(',', ''))
-    data = data.apply(lambda x: str(x).replace('•', ''))
-    data = data.apply(lambda x: str(x).replace('   ', ' '))
-    data = data.apply(lambda x: str(x).replace('  ', ' '))
-    data = data.apply(lambda x: str(x).replace('=', ''))
-    data = data.apply(lambda x: str(x).replace('+', ''))
-
-    data = data.apply(lambda x: '' if type(x) is int or x is float else x)
-
-    def remove_if_num(x: str) -> str:
-        arr = x.split(' ')
-        for i in range(len(arr)):
-            if arr[i].isdigit():
-                x = x.replace(arr[i], '')
-        return x
-
-    data = data.apply(lambda x: remove_if_num(x).strip())
-
-    data = data.str.lower()
-    return data
-
-
-def clean_sample(txt="Sample Text"):
-    txt = str(txt).replace('\n', ' ')
-    txt = str(txt).replace('\'m', ' am')
-    txt = txt.replace('n\'t', ' not')
-    txt = txt.replace('can\'t', 'can not')
-    txt = txt.replace('\'d', ' would')
-    txt = txt.replace(':', ' ')
-    txt = txt.replace('#', ' ')
-    txt = txt.replace('_', ' ')
-    txt = txt.replace('\'s', '')
-    txt = txt.replace('-', ' ')
-    txt = txt.replace('"', '')
-    txt = txt.replace('\'', ' ')
-    txt = txt.replace('!', '')
-    txt = txt.replace('?', '')
-    txt = txt.replace('/', ' ')
-    txt = txt.replace('\'', '')
-    txt = txt.replace('[', '')
-    txt = txt.replace(']', '')
-    txt = txt.replace('{', '')
-    txt = txt.replace('}', '')
-    txt = txt.replace('(', '')
-    txt = txt.replace(')', '')
-    txt = txt.replace('.', '')
-    txt = txt.replace(',', '')
-    txt = txt.replace('•', '')
-    txt = txt.replace('   ', ' ')
-    txt = txt.replace('  ', ' ')
-    txt = txt.replace('=', '')
-    txt = txt.replace('+', '')
-    txt = txt.replace('|', ' ')
-
-    return txt.lower().strip()
